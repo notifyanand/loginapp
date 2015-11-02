d27659460a670d969c03e4f702e80aab68c55111
notifyanand <notifyanand@gmail.com>
2015-11-02 08:12:30 -0800

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+//    'url' : 'mongodb://localhost/localUsers' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://anandm:anandm@ds049104.mongolab.com:49104/CloudFoundry_j07ehhe7_32m3p2ts'
+}
