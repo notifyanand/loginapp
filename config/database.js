fdfa86804d42646b64a44a61925c5ad9a989bb34
notifyanand <notifyanand@gmail.com>
2015-11-02 12:26:04 -0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@
 module.exports = {
 
 //    'url' : 'mongodb://localhost/localUsers' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url' : 'mongodb://anandm:anandm@ds049104.mongolab.com:49104/CloudFoundry_j07ehhe7_32m3p2ts'
+    'url' : 'mongodb://user:password@hostname.mongolab.com:port/CloudFoundry_GUID'
 }
